76c99982b239364de157f969000d05aa032496a4
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-02-22 16:57:38 +0900

    Update users.rb

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,33 +1,15 @@
-ActiveAdmin.register User, as: "MyInfo" do
-  permit_params :email, :username, :password, :password_confirmation, :role
-
-  index do
-    selectable_column
-    id_column
-    column :role
-    column :email
-    column :username
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
-  end
-
-  filter :email
-  filter :username
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
-  form do |f|
-    f.inputs do
-      f.input :email
-      f.input :username
-      f.input :password
-      f.input :password_confirmation
-
-    end
-    f.actions
-  end
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
 
 end
