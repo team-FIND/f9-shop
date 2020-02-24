5231943e61c471df90ad514a74a621d56960cd4b
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-02-24 15:53:05 +0900

    Update myhshops_controller.rb

--- app/controllers/myhshop/myhshops_controller.rb
+++ app/controllers/myhshop/myhshops_controller.rb
@@ -7,12 +7,12 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def index
     @user = User.where(:id => params[:user_id]).first
     $hshops = current_user.hshops.all
-    render :layout => "app2"
+    render :layout => "search"
   end
 
   def show
     $hshop = Hshop.where(:id => params[:id]).first
-    render :layout => "shop/bshop/home"
+    render :layout => "shop_home"
   end
 
   def contact
