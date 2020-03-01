d8859e4fe32ed3b80f18acccfaf983c57b15ed0e
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-01 21:11:16 +0900

    Update bsshops_controller.rb

--- app/controllers/bsearch/bsshops_controller.rb
+++ app/controllers/bsearch/bsshops_controller.rb
@@ -22,19 +22,19 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def contact
-    render :layout => "shop/ashop/contents"
+    render :layout => "shop_contents"
   end
 
   def access
-    render :layout => "shop/ashop/contents"
+    render :layout => "shop_contents"
   end
 
   def time
-    render :layout => "shop/ashop/contents"
+    render :layout => "shop_contents"
   end
 
   def info
-    render :layout => "shop/ashop/contents"
+    render :layout => "shop_contents"
   end
 
   def create
