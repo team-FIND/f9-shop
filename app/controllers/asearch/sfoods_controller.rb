be2abd372ddb15ca3f892b12d68bd5bf01997932
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-01 21:10:40 +0900

    Update sfoods_controller.rb

--- app/controllers/asearch/sfoods_controller.rb
+++ app/controllers/asearch/sfoods_controller.rb
@@ -7,7 +7,7 @@ class Asearch::SfoodsController < ApplicationController
   def index
     $amenu = Amenu.where(:id => params[:asmenu_id]).first
     $foods = $amenu.foods.all
-    render :layout => "shop/ashop/contents"
+    render :layout => "menu_category"
   end
 
   def show
