ab3092e0338b48f5272b2806ce6177343894ba5e
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-07 19:08:10 +0900

    Update picture_uploader.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,14 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.development? # 開発中もs3使う
     config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'f9-shop.jp.img'
-    config.asset_host = 'https://s3.amazonaws.com/f9-shop.jp.img'
+    config.fog_provider = ''
+    config.fog_directory  = ''
+    config.asset_host = ''
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAZYWS7APLUNWK3GWW',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1',
+      aws_access_key_id: '',
+      aws_secret_access_key: '',
+      region: '',
       # path_style: true
     }
   else
