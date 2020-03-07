12a30fb1016c5ccbadf9e2a6ab033047e7de096f
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-07 16:54:16 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-unless Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: 'AKIAZYWS7APLUNWK3GWW',
-        aws_secret_access_key: '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',
-        region: 'ap-northeast-1'
-      }
-  
-      config.fog_directory  = 'f9-shop.jp.img'
-      config.cache_storage = :fog
-    end
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'f9-shop.jp.img'
+    config.asset_host = 'https://s3.amazonaws.com/f9-shop.jp.img'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAZYWS7APLUNWK3GWW',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1',
+      # path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
