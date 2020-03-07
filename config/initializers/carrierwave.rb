3e026797c1f287b307fc6b4417936d976678d5d7
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-07 16:07:45 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,12 @@ unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: '',
-        aws_secret_access_key: '',
-        region: ''
+        aws_access_key_id: 'AKIAZYWS7APLUNWK3GWW',
+        aws_secret_access_key: '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',
+        region: 'ap-northeast-1'
       }
   
-      config.fog_directory  = ''
+      config.fog_directory  = 'f9-shop.jp.img'
       config.cache_storage = :fog
     end
   end
