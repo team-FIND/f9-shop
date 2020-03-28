1aaf408d8cef3c3f81bfbdd4298ed02268cba013
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-03-29 04:01:08 +0900

    Update gsstaffcs_controller.rb

--- app/controllers/gsearch/gsstaffcs_controller.rb
+++ app/controllers/gsearch/gsstaffcs_controller.rb
@@ -1,4 +1,4 @@
-class Gsearch::BGsstaffcsController < ApplicationController
+class Gsearch::GsstaffcsController < ApplicationController
     before_action :set_gstaffc, only: [:show, :edit, :update, :destroy, :subedit]
     protect_from_forgery except: :create
     before_action :set_current_user
@@ -14,7 +14,7 @@ class Gsearch::BGsstaffcsController < ApplicationController
     end
   
     private
-  ßßß
+  
       def set_gstaffc
         $gsshop = Gshop.where(:id => params[:gsshop_id]).first
         $gsstaffc = $gshop.gstaffcs.where(:id => params[:id]).first
