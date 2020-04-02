9f562d70ebe0c01def74c3bf477c3721b75e537c
f9-shop-creator1 <60535006+f9-shop@users.noreply.github.com>
2020-04-03 06:31:07 +0900

    Create <% if $prefec.adshop_name1.present? %>.rb

---
+++ app/views/prefecs/<% if $prefec.adshop_name1.present? %>.rb
@@ -0,0 +1,59 @@
+<% if $prefec.adshop_name1.present? %>
+    <% @recomadname1 = $prefec.adshop_name1 %>
+<% else %>
+    <% @recomadname1 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name2.present? %>
+    <% @recomadname2 = $prefec.adshop_name2 %>
+<% else %>
+    <% @recomadname2 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name3.present? %>
+    <% @recomadname3 = $prefec.adshop_name3 %>
+<% else %>
+    <% @recomadname3 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name4.present? %>
+    <% @recomadname4 = $prefec.adshop_name4 %>
+<% else %>
+    <% @recomadname4 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name5.present? %>
+    <% @recomadname5 = $prefec.adshop_name5 %>
+<% else %>
+    <% @recomadname5 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name6.present? %>
+    <% @recomadname6 = $prefec.adshop_name6 %>
+<% else %>
+    <% @recomadname6 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name7.present? %>
+    <% @recomadname7 = $prefec.adshop_name7 %>
+<% else %>
+    <% @recomadname7 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name8.present? %>
+    <% @recomadname8 = $prefec.adshop_name8 %>
+<% else %>
+    <% @recomadname8 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name9.present? %>
+    <% @recomadname9 = $prefec.adshop_name9 %>
+<% else %>
+    <% @recomadname9 = "募集中" %>
+<% end %>
+
+<% if $prefec.adshop_name10.present? %>
+    <% @recomadname10 = $prefec.adshop_name10 %>
+<% else %>
+    <% @recomadname10 = "募集中" %>
+<% end %>
